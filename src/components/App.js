b3ab79b28f1da9c7a78920d0b8d217200c0ccef3
swetha-A130 <swethachintalapudi6@gmail.com>
2024-11-23 16:57:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,8 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-
+impoert React from "react";
+const App=()=>{
+  return {
+    <div id="root">
+    <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    </div>
+}
+export default App;
